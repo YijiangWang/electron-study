33e72e6d05220df0e0d430cfa8d9376c2f108216
YIJIANGWANG\Administrator <wangyijiang@unipus.cn>
2019-04-09 15:25:31 +0800

    在第一版上进行

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const {app, BrowserWindow} = require('electron');
 
+// 定义一个全局的 window
+let win;
+
 const createWindow = () => {
-  // 创建浏览器窗口
-  let win = new BrowserWindow({width: 360, height: 600});
-  win.loadURL('https://www.baidu.com');
+  win = new BrowserWindow({width: 600, height: 600});
+
+  win.loadFile('index.html');
+  // 打开开发者工具
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  })
 }
 
 app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if(process.platform !== 'darwin'){
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if(win === null){
+    createWindow();
+  }
+})
