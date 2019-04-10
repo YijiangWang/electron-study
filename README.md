908b537d0f8a42323d0f1541d3d50b921479d478
YIJIANGWANG\Administrator <wangyijiang@unipus.cn>
2019-04-10 09:02:22 +0800

    工具栏学习

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # electron-study
 The way to learn electron.
+
+### 主进程和渲染进程
+##### 主进程
+ - package.json 里 main 脚本的进程被称为主进程；
+##### 渲染进程
+ - 由于 Electron 使用 Chromium 来展示页面，Chromium 的多进程结构也被充分利用。每个 Electron 的页面都在运行着自己的进程，这样的进程我们称之为渲染进程
+##### 区别与联系
+ - 主进程使用 BrowserWindow 实例创建网页。每个 BrowserWindow 实例都在自己的渲染进程里运行着一个网页。当一个 BrowserWindow 实例被销毁后，相应的渲染进程也会被终止。
+ - 如果你想在网页里使用 GUI 操作，其对应的渲染进程必须与主进程进行通讯，请求主进程进行相关的 GUI 操作。
+
+### 设置工具栏
+- 设置缩略图工具栏，其中 icon 为必填属性；
+```
+win.setThumbarButtons([
+  {
+    tooltip: "button1",
+    icon: path.join(__dirname, 'file.png'),
+    click: function () { console.log("button1 clicked"); }
+  },
+  {
+    tooltip: "button1",
+    icon: path.join(__dirname, 'audio.png'),
+    click: function () { console.log("button2 clicked"); }
+  }
+]);
+```
+- 设置进度条工具栏
+```
+win.setProgressBar(0.3);
+```
+
+### 辅助功能
+- Spectron、Devtron；
+- 将审查工具带到 electron 应用中。
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
 // 定义一个全局的 window
 let win;
 
 const createWindow = () => {
-  win = new BrowserWindow({width: 600, height: 600});
+  //创建窗口
+  win = new BrowserWindow({ width: 600, height: 600, show: false });
+
+  // 设置缩略图工具栏
+  win.setThumbarButtons([
+    {
+      tooltip: "button1",
+      icon: path.join(__dirname, 'file.png'),
+      click: function () { 
+        console.log("button1 clicked"); 
+        win.setRepresentedFilename('/test.html');
+        win.setDocumentEdited(true);
+      }
+    },
+    {
+      tooltip: "button1",
+      icon: path.join(__dirname, 'audio.png'),
+      click: function () { console.log("button2 clicked"); }
+    }
+  ]);
+
+  // 设置进度条工具栏
+  win.setProgressBar(0.3);
 
   win.loadFile('index.html');
   // 打开开发者工具
@@ -13,18 +36,22 @@ const createWindow = () => {
   win.on('closed', () => {
     win = null;
   })
+
 }
 
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
-  if(process.platform !== 'darwin'){
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 })
 
 app.on('activate', () => {
-  if(win === null){
+  if (win === null) {
     createWindow();
   }
 })
+
